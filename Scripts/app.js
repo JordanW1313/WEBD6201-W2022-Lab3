97bf3114227d027410a8ff1d00bef0b671f5252b
Dakota Parrish <needforspeeddj@gmail.com>
2022-03-19 14:18:42 -0400

    Added task-list to ActiveLinkCallBack function

--- Scripts/app.js
+++ Scripts/app.js
@@ -343,6 +343,7 @@
             case "login": return DisplayLoginPage;
             case "register": return DisplayRegisterPage;
             case "404": return Display404Page;
+            case "task-list": return DisplayTaskList;
             default:
                 console.error("ERROR: callback does not exist: " + router.ActiveLink);
                 return new Function();
