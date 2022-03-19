4c76fd43108f6704debe57638d631a1cf732aa9b
Dakota Parrish <needforspeeddj@gmail.com>
2022-03-19 17:40:30 -0400

    reverted changes

--- Scripts/app.js
+++ Scripts/app.js
@@ -3,6 +3,7 @@
     function AuthGuard() {
         let protected_routes = [
             "contact-list",
+            "task-list"
         ];
         if (protected_routes.indexOf(router.ActiveLink) > -1) {
             if (!sessionStorage.getItem("user")) {
