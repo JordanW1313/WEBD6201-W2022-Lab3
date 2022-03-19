1dadf496a177d4a0de06deb10e7cd6e33211e56f
Dakota Parrish <needforspeeddj@gmail.com>
2022-03-19 16:05:03 -0400

    fixed appending new task issue

--- Scripts/app.js
+++ Scripts/app.js
@@ -299,7 +299,7 @@
                <input type="text" class="form-control edit-task editTextInput">
                </li>
                `;
-            $("#taskList").append(newElement);
+            $("#task-List").append(newElement);
             messageArea.removeAttr("class").hide();
             taskInput.val("");
         }
